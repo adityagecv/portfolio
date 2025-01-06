e349fba5e1c187fb532bc3fee359741cf6f37160
Aditya Kumar <aditya.gecv21@gmail.com>
2025-01-06 21:14:25 +0530

    changed spelling

--- src/constants/index.js
+++ src/constants/index.js
@@ -254,7 +254,7 @@ const projects = [
       },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/adityagecv/Movie-Recommender",
   },
 ];
 
